3ca14e52edf897f1e1278c6f84f8ff604ae79b36
Stepan <56435489+Stepan-Didukh@users.noreply.github.com>
2020-01-28 13:18:54 +0200

    Update

--- src/Container/HomePage/HomePage.js
+++ src/Container/HomePage/HomePage.js
@@ -13,7 +13,8 @@ class HomePage extends Component {
 
     componentDidMount() {
         window.addEventListener('scroll', () => {
-            const isTop = window.scrollY < 100;
+            const isTop = window.scrollY <10 ;
+
             if (isTop !== true) {
                 this.setState({scroller: true});
             } else {
