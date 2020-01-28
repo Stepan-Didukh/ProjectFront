5af81440177c39ae53401842fab24a0dce5a2495
Oleh <olegflud5@gmail.com>
2020-01-28 13:23:35 +0200

    change name

--- src/Container/LoginPage/LoginPage.js
+++ src/Container/LoginPage/LoginPage.js
@@ -1,13 +1,88 @@
-import React, {Component} from "react";
+import React from "react";
+// import loginImg from "../../login.svg";
+import './Login.scss'
+import {
+    Link
+} from 'react-router-dom';
+import {login} from '../../Components/UserFunctions/UserFunctions'
 
-class LoginPage extends Component {
+export default class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        login(user).then(res => {
+            if (res) {
+                this.props.history.push(`/moviesPage`)
+            }
+        })
+    }
+
     render() {
         return (
-            <div>
 
+            <div className='loginRegister'>
+                <div className='container'>
+                    <form noValidate onSubmit={this.onSubmit}>
+
+                        <div className="base-container">
+                            <div className="header">Login</div>
+                            <div className="content">
+                                {/*<div className="image">*/}
+                                {/*    <img src={loginImg}/>*/}
+                                {/*</div>*/}
+                                <div className="form">
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input type="email"
+                                               name="email"
+                                               placeholder="email"
+                                               value={this.state.email}
+                                               onChange={this.onChange}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <input type="password"
+                                               name="password"
+                                               placeholder="password"
+                                               value={this.state.password}
+                                               onChange={this.onChange}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="login-footer">
+                                <button type="submit"
+                                        className="btn">
+                                    Login
+                                </button>
+
+                                <Link to="/register" className={`btnRegister`}>Register</Link>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-        )
+        );
     }
 }
-
-export default LoginPage
