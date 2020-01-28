40879effc92580aa1fbca738aad40e47ed26430f
Stepan <56435489+Stepan-Didukh@users.noreply.github.com>
2020-01-28 11:19:35 +0200

    Fixed routers.

--- src/Container/HomePage/HomePage.js
+++ src/Container/HomePage/HomePage.js
@@ -24,7 +24,7 @@ class HomePage extends Component {
     }
 
     componentWillUnmount() {
-        window.removeEventListener('scroll');
+        window.removeEventListener('scroll',()=>{});
     }
 
     render() {
